02dbc7e68d90d95e6316e7d2ff14cabbe252c6c0
Lanh Le <lanhktc@gmail.com>
2020-04-27 16:48:15 +0700

    no message

--- config/cache.php
+++ config/cache.php
@@ -99,7 +99,7 @@ return [
 
     'prefix' => env(
         'CACHE_PREFIX',
-        Str::slug((env('APP_NAME', 'laravel'), '_').'_cache'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_cache',
     ),
 
 ];
