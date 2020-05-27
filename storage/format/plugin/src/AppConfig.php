8c5995b36e7cc35347108a8d7b552c8d4cadaa6f
Lanh Le <lanhktc@gmail.com>
2020-05-27 22:16:48 +0700

    no message

--- storage/format/plugin/src/AppConfig.php
+++ storage/format/plugin/src/AppConfig.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Format plugin for S-Cart 4.x
+ * Format plugin for S-Cart 2.x
  */
 #App\Plugins\Plugin_Code\Plugin_Key\AppConfig.php
 namespace App\Plugins\Plugin_Code\Plugin_Key;
@@ -12,14 +12,19 @@ class AppConfig extends ConfigDefault
 {
     public function __construct()
     {
+        //Read config from config.json
         $config = file_get_contents(__DIR__.'/config.json');
-    	$config = json_decode($config, true);
+        $config = json_decode($config, true);
     	$this->configGroup = $config['configGroup'];
     	$this->configCode = $config['configCode'];
-    	$this->configKey = $config['configKey'];
+        $this->configKey = $config['configKey'];
+        //Path
         $this->pathPlugin = $this->configGroup . '/' . $this->configCode . '/' . $this->configKey;
+        //Language
         $this->title = trans($this->pathPlugin.'::lang.title');
+        //Image logo or thumb
         $this->image = $this->pathPlugin.'/'.$config['image'];
+        //
         $this->version = $config['version'];
         $this->auth = $config['auth'];
         $this->link = $config['link'];
@@ -30,8 +35,10 @@ class AppConfig extends ConfigDefault
         $return = ['error' => 0, 'msg' => ''];
         $check = AdminConfig::where('key', $this->configKey)->first();
         if ($check) {
+            //Check Plugin key exist
             $return = ['error' => 1, 'msg' => 'Plugin exist'];
         } else {
+            //Insert plugin to config
             $process = AdminConfig::insert(
                 [
                     'group' => $this->configGroup,
@@ -42,6 +49,28 @@ class AppConfig extends ConfigDefault
                     'detail' => $this->pathPlugin.'::lang.title',
                 ]
             );
+
+            /*Insert plugin's html elements into index of admin pages*/
+
+            // AdminConfig::insert(
+            //     [
+            //         /*
+            //         This is where the html content of the Plugin appears
+            //         group_of_layout allow:
+            //         Position include "topMenuRight, topMenuLeft, menuLeft,menuRight, blockBottom" -> Show on all index pages in admin with corresponding position as above.
+            //         or Position_route_name_of_admin_page. Example menuLeft__admin_product.index, topMenuLeft__admin_order.index
+            //         */
+            //         'group' => 'group_of_layout',
+            //         /*
+            //         code is value option
+            //         */
+            //         'code' => 'code_config_of_plugin',
+            //         'key' => 'key_with_value_unique', //
+            //         'sort' => 0, // int value
+            //         'value' => 'html content or view::path_to_view', // allow html or view::path_to_view
+            //         'detail' => '',
+            //     ]
+            // );
             if (!$process) {
                 $return = ['error' => 1, 'msg' => 'Error when install'];
             } else {
@@ -55,6 +84,7 @@ class AppConfig extends ConfigDefault
     public function uninstall()
     {
         $return = ['error' => 0, 'msg' => ''];
+        //Please delete all values inserted in the installation step
         $process = (new AdminConfig)->where('key', $this->configKey)->delete();
         if (!$process) {
             $return = ['error' => 1, 'msg' => 'Error when uninstall'];
@@ -114,6 +144,6 @@ class AppConfig extends ConfigDefault
      *
      */
     public function endApp($data = []) {
-        
+        //action after end app
     }
 }
