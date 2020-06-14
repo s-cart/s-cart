35f448886524b9ad41b9730bc826f24f9e17d90e
Lanh Le <lanhktc@gmail.com>
2020-06-14 16:01:04 +0700

    no message

--- resources/views/templates/default-new/screen/shop_checkout.blade.php
+++ resources/views/templates/default-new/screen/shop_checkout.blade.php
@@ -129,20 +129,21 @@ Use paginate: $products->appends(request()->except(['page','_token']))->links()
                                 <div class="col-md-12">
                                     <table class="table box table-bordered" id="showTotal">
                                         @foreach ($dataTotal as $key => $element)
-                                        @if ($element['value'] !=0)
-
                                         @if ($element['code']=='total')
                                         <tr class="showTotal" style="background:#f5f3f3;font-weight: bold;">
-                                            @else
+                                            <th>{!! $element['title'] !!}</th>
+                                            <td style="text-align: right" id="{{ $element['code'] }}">
+                                                {{$element['text'] }}
+                                            </td>
+                                        </tr>
+                                        @elseif($element['value'] !=0)
                                         <tr class="showTotal">
-                                            @endif
                                             <th>{!! $element['title'] !!}</th>
                                             <td style="text-align: right" id="{{ $element['code'] }}">
                                                 {{$element['text'] }}
                                             </td>
                                         </tr>
                                         @endif
-
                                         @endforeach
                                     </table>
                                     {{-- Payment method --}}
