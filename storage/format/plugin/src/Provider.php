536f3b8af750789ea3fe2c570146acdd78602bdb
Lanh Le <lanhktc@gmail.com>
2020-06-14 08:20:08 +0700

    Add config for plugin

--- storage/format/plugin/src/Provider.php
+++ storage/format/plugin/src/Provider.php
@@ -1,3 +1,6 @@
 <?php
     $this->loadTranslationsFrom(__DIR__.'/Lang', 'Plugins/Plugin_Code/Plugin_Key');
     $this->loadViewsFrom(__DIR__.'/Views', 'Plugins/Plugin_Code/Plugin_Key');
+    // $this->mergeConfigFrom(
+    //     __DIR__.'/config.php', 'key_define_for_plugin'
+    // );
