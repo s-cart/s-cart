87c5cf8a9911d98bf391c9bcf5747a5f19d5355b
Lanh Le <lanhktc@gmail.com>
2020-08-20 20:56:21 +0700

    no message

--- config/lfm.php
+++ config/lfm.php
@@ -201,6 +201,19 @@ return [
             ],
         ],
 
+        'cms' => [
+            'folder_name' => 'cms',
+            'startup_view' => 'grid',
+            'max_size' => 50000, // size in KB
+            'valid_mime' => [
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/gif',
+                'image/svg+xml',
+            ],
+        ],
+
         'file' => [
             'folder_name' => 'file',
             'startup_view' => 'list',
