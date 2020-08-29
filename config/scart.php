5fc3d7273254ae680d825af9c806a3f2c101367e
Lanh Le <lanhktc@gmail.com>
2020-08-29 19:02:51 +0700

    no message

--- config/scart.php
+++ config/scart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '3.0',
-    'sub_version' => '3.0.0',
+    'sub_version' => '3.0.1',
     'homepage'    => 'https://s-cart.org',
     'name'        => 'S-Cart',
     'title'       => 'Free Open Source eCommerce for Business',
