f03df7ab93d7b18cba2e76e14c3bbb9c4882df33
Lanh Le <lanhktc@gmail.com>
2020-09-11 19:50:49 +0700

    update version 3.0.6

--- config/scart.php
+++ config/scart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '3.0',
-    'sub_version' => '3.0.5',
+    'sub_version' => '3.0.6',
     'homepage'    => 'https://s-cart.org',
     'name'        => 'S-Cart',
     'title'       => 'Free Open Source eCommerce for Business',
