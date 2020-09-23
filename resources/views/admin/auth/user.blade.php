ea444225fe8df5431a97176c932a4a0fb33db97c
Lanh Le <lanhktc@gmail.com>
2020-09-23 22:36:42 +0700

    no message

--- resources/views/admin/auth/user.blade.php
+++ resources/views/admin/auth/user.blade.php
@@ -218,7 +218,7 @@
 
                         {{-- select store --}}
                         @if (count($stores) > 1)
-                            @if ($isAllStore)
+                            @if (!empty($isAllStore))
                             <label for="store" class="col-sm-2 col-form-label">
                                 {{ trans('store.select_store') }}
                             </label>
