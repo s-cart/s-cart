f634c1fa58cd1d5c1668c774b1b889d3026e1430
Lanh Le <lanhktc@gmail.com>
2020-10-20 05:40:44 +0700

    no message

--- resources/views/templates/s-cart-light/block/categories_store.blade.php
+++ resources/views/templates/s-cart-light/block/categories_store.blade.php
@@ -1,13 +1,15 @@
 @php
 $storeId = empty($storeId) ? config('app.storeId') : $storeId;
-$listSubCategory = $modelSubCategory->start()->setStore($storeId)->getData();
+if(isset($modelCategoryStore)) {
+  $listCategoryStore = $modelCategoryStore->start()->setStore($storeId)->getData();
+}
 @endphp
 
-@if (!empty($listSubCategory) && $listSubCategory->count())
+@if (!empty($listCategoryStore) && $listCategoryStore->count())
 <div class="aside-item col-sm-6 col-md-5 col-lg-12">
   <h6 class="aside-title">{{ trans('front.sub_categories') }}</h6>
   <ul class="list-shop-filter">
-    @foreach ($listSubCategory as $key => $category)
+    @foreach ($listCategoryStore as $key => $category)
     <li class="product-minimal-title active"><a href="{{ $category->getUrl() }}"> {{ $category->getTitle() }}</a></li>
     @endforeach
   </ul>
