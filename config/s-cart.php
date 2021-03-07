a97ebd1c8e3c2c669d690cedb21a8de81761431c
Lanh Le <lanhktc@gmail.com>
2021-03-07 13:38:47 +0700

    Update config

---
+++ config/s-cart.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'version'     => '6.0',
+    'sub-version' => '6.0-beta',
+    'type'        => 'basic',
+    'homepage'    => 'https://s-cart.org',
+    'title'       => 'Free Open Source eCommerce for Business',
+];
