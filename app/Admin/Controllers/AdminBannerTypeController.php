a7e19df8fe2c2043580ba1bce9779366844aad78
Lanh Le <lanhktc@gmail.com>
2021-03-10 06:05:35 +0700

    Add controller banner type

---
+++ app/Admin/Controllers/AdminBannerTypeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Admin\Controllers;
+
+class AdminBannerTypeController extends \SCart\Core\Admin\Controllers\AdminBannerTypeController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
