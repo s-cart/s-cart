7abef4dd929ae6ebf7c5f27be768a1bbff455d8f
Lanh Le <lanhktc@gmail.com>
2021-03-21 08:02:32 +0700

    no message

--- config/database.php
+++ config/database.php
@@ -53,6 +53,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            'path_mysql_bin' => env('PATH_MYSQL_BIN'),
             'prefix' => '',
             'strict' => false,
             'engine' => null,
