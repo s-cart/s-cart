0ae306c95396bed3ea133d1dd52d9671a733a202
Lanh Le <lanhktc@gmail.com>
2021-07-05 06:08:17 +0700

    Update function discount

--- app/Plugins/Total/Discount/Views/Admin.blade.php
+++ app/Plugins/Total/Discount/Views/Admin.blade.php
@@ -151,7 +151,13 @@
                             {{-- select shop_store --}}
                             @php
                             $listStore = [];
-                            $shop_store = old('shop_store', sc_get_list_store_of_discount_detail($discount['id'] ?? ''));
+                            if (function_exists('sc_get_list_store_of_discount_detail')) {
+                                $oldData = sc_get_list_store_of_discount_detail($discount['id'] ?? '');
+                            } else {
+                                $oldData = null;
+                            }
+                            $shop_store = old('shop_store', $oldData);
+
                             if(is_array($shop_store)){
                                 foreach($shop_store as $value){
                                     $listStore[] = (int)$value;
