b57bab8e025edafb90d97ed7c07470d16f323599
S-Cart <giaiphap247.com@gmail.com>
2021-08-22 16:27:27 +0700

    Remove config pasport from front

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laravel\Passport\Passport;
 use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,7 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //Dont use migrate from library passport
-        Passport::ignoreMigrations();
+        //
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,10 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
-use Laravel\Passport\Console\ClientCommand;
-use Laravel\Passport\Console\InstallCommand;
-use Laravel\Passport\Console\KeysCommand;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -17,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -29,19 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
-        Passport::routes();
-        Passport::tokensExpireIn(now()->addDays(15));
-
-        Passport::refreshTokensExpireIn(now()->addDays(30));
-
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
-
-        /*ADD THIS LINES*/
-        $this->commands([
-            InstallCommand::class,
-            ClientCommand::class,
-            KeysCommand::class,
-        ]);
     }
 }
