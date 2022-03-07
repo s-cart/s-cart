251b5f33ee0762579a809113cacf3292919efc84
S-Cart <giaiphap247.com@gmail.com>
2022-03-08 06:01:19 +0700

    Support price decimal

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '6.9',
-    'sub-version' => '6.9.1',
+    'sub-version' => '6.9.2',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
