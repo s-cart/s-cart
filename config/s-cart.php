6d450dafcee8a0cddb487508eb98e1bf2382863b
S-Cart <giaiphap247.com@gmail.com>
2022-03-17 06:58:51 +0700

    Support Laravel 9

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '6.9',
-    'sub-version' => '6.9.3',
+    'version'     => '7.0',
+    'sub-version' => '7.0-beta',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
