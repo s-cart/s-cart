8974e9687fdcb5ab3c90099d827837a7aa246a76
S-Cart <giaiphap247.com@gmail.com>
2022-05-03 15:02:28 +0700

    Revert auth controller

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+class ForgotPasswordController extends \SCart\Core\Front\Controllers\Auth\ForgotPasswordController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+class ResetPasswordController extends \SCart\Core\Front\Controllers\Auth\ResetPasswordController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
