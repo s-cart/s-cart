0369bd6497f2e68ca0c3dd006cc495c567e167d0
S-Cart <giaiphap247.com@gmail.com>
2022-08-20 16:22:16 +0700

    Update version

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '7.1',
-    'sub-version' => '7.1.1',
+    'sub-version' => '7.1.2',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
