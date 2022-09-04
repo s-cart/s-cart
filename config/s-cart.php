0b1d6644d8b799f571697a91cd0d46ee668d3946
S-Cart <giaiphap247.com@gmail.com>
2022-09-04 18:51:40 +0700

    S-Cart 7.2-beta

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '7.1',
-    'sub-version' => '7.1.3',
+    'version'     => '7.2',
+    'sub-version' => '7.2-beta',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
