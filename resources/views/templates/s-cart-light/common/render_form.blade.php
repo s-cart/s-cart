50e3232ab8f4351dd6b5bd5de2e3ea821a01c6fb
S-Cart <tech@s-cart.org>
2023-02-04 21:14:05 +0700

    Support custom field

---
+++ resources/views/templates/s-cart-light/common/render_form.blade.php
@@ -0,0 +1,32 @@
+@if (isset($customFields) && count($customFields))
+                <hr class="kind ">
+                    @foreach ($customFields as $keyField => $field)
+                    <div class="form-group row kind   {{ $errors->has('fields.'.$field->code) ? ' text-red' : '' }}">
+                        <label class="col-sm-2 col-form-label">{{ sc_language_render($field->name) }}</label>
+                        <div class="col-sm-8">
+                        @php
+                            $default  = json_decode($field->default, true);
+                            $dataForm = [
+                                'name' => ($field->option == 'checkbox') ?'fields['.$field->code.'][]' : 'fields['.$field->code.']',
+                                'type' => $field->option,
+                                'attribute' => '',
+                                'placeholder' => '',
+                                'class' => 'input-sm '.$field->code,
+                                'id' => $keyField,
+                                'default' => old('fields.'.$field->code, ($fields[$field->code]['text'] ?? '')),
+                                'dataFormat' => $default,
+                                'css' => 'width: 100%;',
+                                'required' =>  $field->required,
+                            ];
+                        @endphp
+                        {!! sc_form_render_field($dataForm) !!}
+
+                            @if ($errors->has('fields.'.$field->code))
+                            <span class="form-text">
+                                <i class="fa fa-info-circle"></i> {{ $errors->first('fields.'.$field->code) }}
+                            </span>
+                            @endif
+                        </div>
+                    </div>
+                    @endforeach
+@endif
