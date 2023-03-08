1d8f2172b062157a0d366e84d30c3ca05ab85967
S-Cart <tech@s-cart.org>
2023-03-09 05:47:07 +0700

    Update core

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '8.0-beta',
-    'sub-version' => '8.0.-beta.1',
+    'version'     => '8.0',
+    'sub-version' => '8.0-beta',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
