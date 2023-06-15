bad7a34e3f5d24b24d7eae3543917254bcb5ae8b
zamanshahed <shaheduz.zaman@northsouth.edu>
2023-06-15 09:21:51 +0600

    new footer design implemented according to figma, with responsive

--- resources/views/templates/s-cart-light/block_footer.blade.php
+++ resources/views/templates/s-cart-light/block_footer.blade.php
@@ -1,110 +1,62 @@
       <!-- Page Footer-->
       <footer class="section footer-classic">
-        <div class="footer-classic-body section-sm bg-brown-2">
-          <div class="container-fluid footer-fix-container">
-            <div class="row row-40 row-md-50 justify-content-xl-between">
-              <div class="col-sm-6 col-lg-4 col-xl-3 wow fadeInRight">
-                <a href="{{ sc_route('home') }}">
-                    <img class="logo-footer" src="{{  sc_file(sc_store('logo', ($storeId ?? null))) }}" alt="{{ sc_store('title', ($storeId ?? null)) }}">
-                </a>
-                <p>{{ sc_store('title', ($storeId ?? null)) }}</p>
-                <p> {!! sc_store('time_active', ($storeId ?? null))  !!}</p>
-                <div class="footer-classic-social footer-fix-hr-line">
-                  <div class="group-lg group-middle">
-                    <div>
-                      <ul class="list-inline list-social list-inline-sm">
-                        @if (sc_config('facebook_url'))
-                        <li><a class="icon mdi mdi-facebook" href="{{ sc_config('facebook_url') }}"></a></li>
-                        @endif
-                        @if (sc_config('twitter_url'))
-                        <li><a class="icon mdi mdi-twitter" href="{{ sc_config('twitter_url') }}"></a></li>
-                        @endif
-                        @if (sc_config('instagram_url'))
-                        <li><a class="icon mdi mdi-instagram" href="{{ sc_config('instagram_url') }}"></a></li>
-                        @endif
-                        @if (sc_config('youtube_url'))
-                        <li><a class="icon mdi mdi-youtube-play" href="{{ sc_config('youtube_url') }}"></a></li>
-                        @endif
-                      </ul>
-                    </div>
+          <div class="footer-classic-body section-sm bg-brown-2">
+              <div class="container">
+                  <div class="footer-fix-container">
+                      {{-- logo --}}
+                      <a class="footer-fix-logo" href="{{ sc_route('home') }}">
+                          <img class="logo-footer" src="{{ sc_file(sc_store('logo', $storeId ?? null)) }}"
+                              alt="{{ sc_store('title', $storeId ?? null) }}">
+                      </a>
+
+                      <div class="footer-fix-data">
+                          {{-- address --}}
+                          <div class="footer-fix-address">
+                              <h4 class="footer-classic-title footer-fix-title">Address</h4>
+                              <a class="" href="#">{{ sc_language_render('store.address') }}:
+                                  {{ sc_store('address', $storeId ?? null) }}</a>
+                          </div>
+
+
+                          {{-- contact --}}
+                          <div class="footer-fix-contact">
+                              <h4 class="footer-classic-title footer-fix-title"> Contact</h4>
+
+                              <a href="tel:{{ sc_store('long_phone', $storeId ?? null) }}">{{ sc_language_render('store.hotline') }}:
+                                  {{ sc_store('long_phone', $storeId ?? null) }}</a>
+
+                              <div class="pt-2">
+                                  <a href="mailto:{{ sc_store('email', $storeId ?? null) }}">{{ sc_language_render('store.email') }}:
+                                      {{ sc_store('email', $storeId ?? null) }}</a>
+                              </div>
+                          </div>
+                      </div>
+
                   </div>
-                </div>
               </div>
-              <div class="col-sm-6 col-lg-4 col-xl-3 wow fadeInRight footer-fix-about-us" data-wow-delay=".1s">
-                <h4 class="footer-classic-title">{{ sc_language_render('about.page_title') }}</h4>
-                <ul class="contacts-creative footer-fix-address footer-fix-title">
-                  <li>
-                    <div class="unit unit-spacing-sm flex-column flex-md-row">
-                      <div class="unit-left"><span class="icon mdi mdi-map-marker"></span></div>
-                      <div class="unit-body"><a href="#">{{ sc_language_render('store.address') }}: {{ sc_store('address', ($storeId ?? null)) }}</a></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="unit unit-spacing-sm flex-column flex-md-row">
-                      <div class="unit-left"><span class="icon mdi mdi-phone"></span></div>
-                      <div class="unit-body"><a href="tel:#">{{ sc_language_render('store.hotline') }}: {{ sc_store('long_phone', ($storeId ?? null)) }}</a></div>
-                    </div>
-                  </li>
-                  <li>
-                    <div class="unit unit-spacing-sm flex-column flex-md-row">
-                      <div class="unit-left"><span class="icon mdi mdi-email-outline"></span></div>
-                      <div class="unit-body"><a href="mailto:#{{ sc_store('email', ($storeId ?? null)) }}">{{ sc_language_render('store.email') }}: {{ sc_store('email', ($storeId ?? null)) }}</a></div>
-                    </div>
-                  </li>
-                  {{-- <li>
-
-                    <form class="rd-form-inline rd-form-inline-2"  method="post" action="{{ sc_route('subscribe') }}">
-                    @csrf
-                      <div class="form-wrap">
-                        <input class="form-input" id="subscribe-form-2-email" type="email" name="subscribe_email" required/>
-                        <label class="form-label" for="subscribe-form-2-email">{{ sc_language_render('subscribe.email') }}</label>
-                      </div>
-                      <div class="form-button">
-                        <button class="button button-icon-2 button-zakaria button-primary" type="submit" title="{{ sc_language_render('subscribe.title') }}">
-                          <span class="fl-bigmug-line-paper122"></span>
-                        </button>
-                      </div>
-                    </form>
-                  </li> --}}
-                </ul>
-              </div>
-              <div class="col-lg-4 wow fadeInRight footer-fix-profile" data-wow-delay=".2s">
-                <h4 class="footer-classic-title"> {{ sc_language_render('front.my_profile') }}</h4>
-                <!-- RD Mailform-->
-                <ul class="contacts-creative footer-fix-title">
-                  @if (!empty(sc_link_collection()['footer']))
-                  @foreach (sc_link_collection()['footer'] as $url)
-                    @if ($url['type'] != 'collection')
-                      <li class="rd-nav-item footer-fix-profile-row">
-                        <a class="rd-nav-link" {{ ($url['data']['target'] =='_blank')?'target=_blank':''  }}
-                            href="{{ sc_url_render($url['data']['url']) }}">{{ sc_language_render($url['data']['name']) }}</a>
-                      </li>
-                    @endif
-                  @endforeach
-                  @endif
-                </ul>
-              </div>
-            </div>
           </div>
-        </div>
 
-        <div class="py-2">
-          <div class="container">
-            <div class="row row-10 align-items-center justify-content-sm-between">
-              <div class="col-md-auto">
-                <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>{{ sc_store('title', ($storeId ?? null)) }}</span><span>.&nbsp; All rights reserved</span></p>
+          <div class="py-2">
+              <div class="container">
+                  <div class="row row-10 align-items-center justify-content-sm-between">
+                      <div class="col-md-auto">
+                          <p class="rights"><span>&copy;&nbsp;</span><span
+                                  class="copyright-year"></span><span>&nbsp;</span><span>{{ sc_store('title', $storeId ?? null) }}</span><span>.&nbsp;
+                                  All rights reserved</span></p>
+                      </div>
+                      @if (sc_config('fanpage_url'))
+                          <div class="col-md-auto order-md-1"> <a target="_blank"
+                                  href="{{ sc_config('fanpage_url') }}">Fanpage FB</a>
+                          </div>
+                      @endif
+                      @if (!sc_config('hidden_copyright_footer'))
+                          <div class="col-md-auto">
+                              Power by <a href="{{ config('s-cart.homepage') }}">{{ config('s-cart.name') }}
+                                  {{ config('s-cart.sub-version') }}</a>
+                          </div>
+                      @endif
+                  </div>
               </div>
-              @if (sc_config('fanpage_url'))
-              <div class="col-md-auto order-md-1"> <a target="_blank"
-                href="{{ sc_config('fanpage_url') }}">Fanpage FB</a>
-              </div>
-              @endif
-              @if (!sc_config('hidden_copyright_footer'))
-              <div class="col-md-auto">
-                    Power by <a href="{{ config('s-cart.homepage') }}">{{ config('s-cart.name') }} {{ config('s-cart.sub-version') }}</a>
-              </div>
-              @endif
-            </div>
           </div>
-        </div>
+
       </footer>
