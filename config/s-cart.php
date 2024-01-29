85656428a72f8bb27d496c867988cb874d6c5e67
S-Cart <tech@s-cart.org>
2024-01-29 13:35:35 +0700

    Revert automatic creation of env files via composer

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'version'     => '8.1',
-    'sub-version' => '8.1.9',
+    'sub-version' => '8.1.10',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
