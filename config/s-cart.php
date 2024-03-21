f457a6d35c38add1dd4a6084d65be500c855ec9e
S-Cart <tech@s-cart.org>
2024-03-22 05:51:49 +0700

    Update LV 11

--- config/s-cart.php
+++ config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '8.1',
-    'sub-version' => '8.1.10',
+    'version'     => '9.0',
+    'sub-version' => '9.0.0',
     'type'        => 'basic',
     'homepage'    => 'https://s-cart.org',
     'title'       => 'Free Open Source eCommerce for Business',
