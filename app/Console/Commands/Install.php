5e9ecea6488344949588b459a8a388e7511eebd0
s-cart <tech@s-cart.org>
2025-07-14 18:06:46 +0700

    Hotfix error install default store

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -29,12 +29,12 @@ class Install extends Command
             $this->info('Installing S-Cart...');
             //Check command gp247:core-install is exist  
             if (array_key_exists('gp247:core-install', Artisan::all())) {
-                $this->info('Installing Core...');
+                $this->info(' - Installing Core...');
                 $this->call('gp247:core-install', ['--force' => 1]);
             }
             //Check command gp247:front-install is exist
             if (array_key_exists('gp247:front-install', Artisan::all())) {
-                $this->info('Installing Front...');
+                $this->info(' - Installing Front...');
                 $this->call('gp247:front-install');
             }
 
@@ -42,6 +42,18 @@ class Install extends Command
             if (array_key_exists('gp247:shop-install', Artisan::all())) {
                 $this->info('Installing Shop...');
                 $this->call('gp247:shop-install');
+            }
+
+            //Setup store with template
+            $this->info(' - Setup store with template...');
+            $template = (defined('GP247_TEMPLATE_FRONT_DEFAULT') ? GP247_TEMPLATE_FRONT_DEFAULT : 'Default');
+            $classTemplate = 'App\GP247\Templates\\'.$template.'\AppConfig';
+            if (class_exists($classTemplate)) {
+                $template = new $classTemplate;
+                if (method_exists($template, 'setupStore')) {
+                    $storeId = GP247_STORE_ID_ROOT;
+                    $template->setupStore($storeId);
+                }
             }
             $this->info('S-Cart installed successfully');
         }
